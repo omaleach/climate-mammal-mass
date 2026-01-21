d081666e39036bf378470667d0c182d3ad3d0ba5
omaleach <omaleach@mun.ca>
2026-01-21 13:38:53 -0330

    add variables

---
+++ code/variables.R
@@ -0,0 +1,6 @@
+#load data
+climate_path <- "data/climate.csv" 
+density_path <- "data/density.csv" 
+spring.hare_path <- "data/spring.hare.csv"
+spring.red_path <- "data/spring.red.csv"
+spring.mice_path <- "data/spring.mice.csv"
